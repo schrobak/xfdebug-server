61de32e1fe3ae1c5be0e6b3170068eff17945804
Sławomir Chrobak <slawomir.chrobak@gmail.com>
2013-07-16 00:37:15 +0200

    Colors in terminal
    Added support for coloring in terminal and also basic imporvement on
    path slicing

--- xfdebug_server.py
+++ xfdebug_server.py
@@ -2,6 +2,8 @@
 
 from __future__ import print_function
 import asyncore
+import colorama
+import os
 import socket
 from pygments import highlight
 from pygments.lexers import PhpLexer
@@ -10,7 +12,7 @@ from pygments.formatters import TerminalFormatter
 
 class XfDebugServer(asyncore.dispatcher):
 
-    def __init__(self, host='localhost', port=2409):
+    def __init__(self, host='127.0.0.1', port=2409):
         asyncore.dispatcher.__init__(self)
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.set_reuse_addr()
@@ -31,19 +33,26 @@ class XfDebugServer(asyncore.dispatcher):
         print("\nClosing server...")
         self.close()
 
-    def handle_write(self):
-        print("Writting in Server")
-
 
 class XfDebugHandler(asyncore.dispatcher_with_send):
 
-    def handle_write(self):
-        print("Writting in Handler")
-
     def handle_read(self):
         data = self.recv(8192)
-        # TODO: Add support for reading information about from which file and line data were send.
+
         if data:
+            colorama.init()
+            lines = data.split("\n")
+            for num, line in enumerate(lines):
+                if '[TRACE]' in line:
+                    type, msg = line.split()
+                    print((msg, os.getcwd()))
+                    if msg.startswith(os.getcwd()):
+                        msg = msg[len(os.getcwd()):]
+                    print(colorama.Back.GREEN + colorama.Fore.WHITE + type, end=' ')
+                    print(colorama.Back.RESET + colorama.Fore.MAGENTA + msg)
+                    del lines[num]
+            data = "\n".join(lines)
+
             lexer = PhpLexer(startinline=True, encoding='utf-8', stripall=True)
             formatter = TerminalFormatter(encoding='utf-8')
-            print(highlight(data, lexer, formatter))
+            print(colorama.Style.RESET_ALL + highlight(data, lexer, formatter))
